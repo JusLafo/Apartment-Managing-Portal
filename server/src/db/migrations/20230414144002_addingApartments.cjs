f799bf087d4974e0677d366da825e1c1c1a8f405
Justin Laforce <jus.laforce@gmail.com>
2023-04-14 10:59:17 -0400

    Added apartments route and created apartment db

---
+++ server/src/db/migrations/20230414144002_addingApartments.cjs
@@ -0,0 +1,24 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+  return knex.schema.createTable("apartments", (table) => {
+    table.bigIncrements("id")
+    table.string("buildingNumber").notNullable()
+    table.string("apartmentNumber").notNullable()
+    table.string("tenantCount").notNullable()
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
+    table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
+  })
+}
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists("apartments")
+}
